901e1859382390866b0b5e9ea385b95f7bf17a87
ALBERTO TORRES <scyanh@gmail.com>
2022-07-05 16:16:39 -0500

    sliding window find average of subarray

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/scyanh/leetcode/stringschallenge"
+	"github.com/scyanh/leetcode/slidingwindow"
 )
 
 func main() {
 	fmt.Println("hello")
 
 	//str:="00110011"
-	str:="clementisacap"
+	//str:="clementisacap"
+	input:=[]int{1, 3, 2, 6, -1, 4, 1, 8, 2}
+	k:=5
 
-	res := stringschallenge.NewStringsChallenge().LongestSubstringWithoutDuplication(str)
+	res := slidingwindow.NewSlidingWindowChallenge().FindAverageOfSubarrayOfSizeK(input, k)
 	fmt.Println("res=", res)
 
 }
