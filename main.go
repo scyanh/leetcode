6566439f9c1dbdef8c0f59482ad0af0e4296348a
ALBERTO TORRES <scyanh@gmail.com>
2022-07-05 20:57:08 -0500

    FindSmallestSubarrayWithGivenSum

--- main.go
+++ main.go
@@ -10,10 +10,10 @@ func main() {
 
 	//str:="00110011"
 	//str:="clementisacap"
-	input:=[]int{1, 3, 2, 6, -1, 4, 1, 8, 2}
-	k:=5
+	input := []int{2, 3, 1, 2, 4, 3}
+	k := 7
 
-	res := slidingwindow.NewSlidingWindowChallenge().FindAverageOfSubarrayOfSizeK(input, k)
+	res := slidingwindow.NewSlidingWindowChallenge().FindSmallestSubarrayWithGivenSum(input, k)
 	fmt.Println("res=", res)
 
 }
