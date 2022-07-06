07453e3c5fbeb35f4d56477048f3969dc3cc4e06
ALBERTO TORRES <scyanh@gmail.com>
2022-07-06 17:13:49 -0500

    remove package

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/scyanh/leetcode/slidingwindow"
 )
 
 func main() {
@@ -11,10 +10,7 @@ func main() {
 	//str:="00110011"
 	//str:="clementisacap"
 	//input := []int{2, 3, 1, 2, 4, 3}
-	input:=[]string{"A","B","C","A","C"}
+	//input:=[]string{"A","B","C","A","C"}
 	//k := 2
-
-	res := slidingwindow.NewSlidingWindowChallenge().MaxFruitsIntoBasket(input)
-	fmt.Println("res=", res)
 
 }
